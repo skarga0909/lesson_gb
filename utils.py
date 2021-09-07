904f34f0eaeca01d0c1bfc2ec94ed47b936c3915
skarga0909 <skarga0909@gmail.com>
2021-09-07 09:30:46 +0300

    D/z lesson 4: 5 files and 6 screenshots

---
+++ utils.py
@@ -0,0 +1,24 @@
+from requests import get, utils
+
+
+def currency_rates():
+    print('Input currency code: ')
+    currency_code = input()
+    response = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    encodings = utils.get_encoding_from_headers(response.headers)
+    content = response.content.decode(encoding=encodings)
+    currency_code = currency_code.upper()
+
+    if currency_code not in content:
+        print('None')
+    else:
+        # print(content)
+        currency = content.split(currency_code)[1]
+        currency_2 = currency.split('</Value>')[0]
+        currency_3 = currency_2.split('<Value>')[1]
+        currency_4 = currency_3.split(',')
+        currency_5 = '.'.join(currency_4)
+        currency_5 = float(currency_5)
+        print(currency_5)
+
+
